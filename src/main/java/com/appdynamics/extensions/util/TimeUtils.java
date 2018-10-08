b74cea2e8f59ccd281cbe686ae9dd858b1c99bd8
Venkata Konala <venkata.konala@appdynamics.com>
2018-10-07 18:39:28 -0700

    Added logging for startTime, endTime and total excution time of a monitor job run

---
+++ src/main/java/com/appdynamics/extensions/util/TimeUtils.java
@@ -0,0 +1,25 @@
+package com.appdynamics.extensions.util;
+
+import com.appdynamics.extensions.ABaseMonitor;
+import com.appdynamics.extensions.logging.ExtensionsLoggerFactory;
+import org.slf4j.Logger;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by venkata.konala on 9/6/18.
+ */
+public class TimeUtils {
+
+    private static final Logger logger = ExtensionsLoggerFactory.getLogger(TimeUtils.class);
+
+    public static String getFormattedTimestamp(Long timeInMilli, String pattern) {
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            return simpleDateFormat.format(timeInMilli);
+        } catch (Exception e) {
+            logger.error("The time " + timeInMilli + " cannot be formatted to the pattern " + pattern);
+        }
+        return null;
+    }
+}
